6854a8518f020cabee4cdc8d266fad1b9e2c6b4d
macerritosz <macerritos@wpi.edu>
2025-02-27 18:33:01 -0500

    Created Sessions, and put the user id as part of the ServerSession

--- src/app/dashboard/timer/page.js
+++ src/app/dashboard/timer/page.js
@@ -28,6 +28,11 @@ export default function Timer() {
     const session = useSession();
     const [dropDown, setDropDown] = useState({}); //Keep a object for each dropdown so multiple can be toggled at once
     const [timerColor, setTimerColor] = useState("text-black"); //state to track color during presses
+
+    /* Current Session Object to allow direct access to the array*/
+    const [currentSession, setCurrentSession] = useState(null);
+    const [selectedSession, setSelectedSession] = useState("3x3");
+
     /* State Handling Key Presses & Timer */
     /*
     const [currentTime, setCurrentTime] = useState(0); //Update for timer start
@@ -39,8 +44,11 @@ export default function Timer() {
     */
 
     useEffect(() => {
-        updateTable()
-    }, []);
+        if (session.status === "authenticated") {
+            router.push('/dashboard/timer');
+        }
+    }, [session.status, router]);
+
 
     useEffect(() => {
         document.addEventListener("keydown", keyDownHandler);
@@ -53,10 +61,21 @@ export default function Timer() {
     }, []);
 
     useEffect(() => {
-        if (session.status === "authenticated") {
-            router.push('/dashboard/timer');
+        const fetchData = async () => {
+            await updateTable()
         }
-    }, [session.status, router]);
+        fetchData();
+    }, []);
+
+    useEffect(() => {
+        if(currentSession !== null && selectedSession !== null)  {setCurrentSession(getSession( selectedSession));}
+    },[selectedSession]);
+
+    /* Runs once when the page loads to set the default Session */
+    useEffect(() => {
+        setCurrentSession(getSession('3x3'));
+    },[])
+
 
     function updateTimerColor() {
         if (running) {
@@ -142,7 +161,7 @@ export default function Timer() {
     }
 
     /*
-    Color Changes will depend on the state, changed to post to data
+        Need to change how data is added to Database with the newly created Session
      */
     async function addTimeToDB(time, timestamp) {
         try {
@@ -154,9 +173,7 @@ export default function Timer() {
                 },
                 body: JSON.stringify(data),
             })
-            //since we dont do anything with response, and errors are in catch statement
             await response.json()
-            //use state to trigger updateTable instead of directly calling it
             await updateTable()
         } catch (err) {
             console.error(' addTimeToDB Error:', error);
@@ -223,6 +240,38 @@ export default function Timer() {
         setDropDown({...dropDown, [id]: !dropDown[id]});
     }
 
+    /**
+     * Creates a sessions with the provided name for the user, and adds a row in the Sessions table with Schema
+     * {MongoID, UserID, SessionName, timeData[]}
+     * @returns {Promise<void>}
+     */
+    async function createSession(sessionName){
+        try{
+            const response = await fetch('/api/sessions', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({sessionName: sessionName}),
+            })
+        } catch (error) {
+            console.error(' createSession Error:', error);
+        }
+    }
+
+    async function getSession(sessionName){
+        try{
+            const response = await fetch(`/api/sessions?sessionName=${sessionName}`, {
+                method: 'GET'
+            })
+            if(response.ok) {
+                return response.json();
+            }
+        } catch (error) {
+            console.error('Error fetching user sessions: ', error);
+        }
+    }
+
     function createTimeData(solve) {
         /*
         Creates a List for each time again,
@@ -259,6 +308,22 @@ export default function Timer() {
         <div className="bg-gray-300 flex flex-col h-screen overflow-hidden">
             <div className="flex flex-1 overflow-hidden">
                 <div className="bg-gray-800 text-white w-64 p-4 overflow-y-auto flex flex-col">
+                    <div className="flex text-black">
+                        <select
+                            value = {selectedSession}
+                            onChange={(e) => setSelectedSession(e.target.value)}>
+                            <option value="3x3" > 3x3 </option>
+                            <option value="5x5" > 5x5 </option>
+                            {
+                                /* State to track the sessions state, which reloads the visible table and other data */
+                                /* Add Option when + is clicked */
+                                /* Only the First 2/3 will have the solver api attached */
+                                /* When the Plus button is clicked open a pop-up to input name information and a submit to call createSession */
+                                /* after its created getSession has to be called aswell */
+                            }
+                        </select>
+                        <button title={"Add Custom Session"} > + </button>
+                    </div>
                     <h2 className="text-4xl font-bold mb-4">Times</h2>
                     <ul id="timelist" className="space-y-2 text-2xl text-center">
                         {
